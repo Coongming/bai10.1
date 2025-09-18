56b40905bc9793ec9620589f94d9fcee67998aae
Văn Đức Công Minh <vanduccongminh@MacBook-Pro-cua-Coongming.local>
2025-09-18 10:51:31 +0700

    Cập nhật mã mới

--- src/main/java/murach/email/EmailListServlet.java
+++ src/main/java/murach/email/EmailListServlet.java
@@ -1,9 +1,11 @@
 package murach.email;
 
-import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import murach.business.User;
 import murach.data.UserDB;
 
